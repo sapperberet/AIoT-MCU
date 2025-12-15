76485f2c51393a0cbc239a9e719e18bc44645a0d
Philopatear Abdelmaseh Abdelfady <philopatearabdelfady96@gmail.com>
2025-12-15 23:23:20 +0200

    Addig the logic to run the code

--- SensorsCode/Application/src/main.cpp
+++ SensorsCode/Application/src/main.cpp
@@ -5,8 +5,7 @@ void setup() {
   Serial.begin(115200); /* Beginning serial monitor to baud 115200 */
   dht.begin();          /* beginning the dht sensor */
 
-  /****************Initializing the mode of each
-   * pin*****************************/
+  /****************Initializing the mode of each pin***************************/
   pinMode(FAN_ENA, OUTPUT);
   pinMode(FAN_IN1, OUTPUT);
   pinMode(FAN_IN2, OUTPUT);
@@ -21,6 +20,7 @@ void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
+  pinMode(PUSH_BUTTON, INPUT_PULLUP);
 
   servoWindow1.attach(SERVO_WINDOW1_PIN);
   servoWindow2.attach(SERVO_WINDOW2_PIN);
@@ -65,68 +65,69 @@ void loop() {
     int flame = digitalRead(FLAME_PIN);        /* Flame */
     int lightLevel = analogRead(LDR_PIN);      /* Light level */
     int rain = digitalRead(RAIN_PIN);          /* Rain */
+    bool faceDetectionRun = buttonPressed(PUSH_BUTTON);  /* Checks if the push button is pressed */
 
-    /* Monitoring all sensors data */
-    Serial.printf("------ SENSORS DATA ------\n");
-    Serial.printf("Temp: %0.3f\n", temperature);
-    Serial.printf("Humidity: %0.3f\n", humidity);
-    Serial.printf("Smoke: %i\n", smoke);
-    Serial.printf("Flame: %i\n", flame);
-    Serial.printf("Light: %i\n", lightLevel);
-    Serial.printf("Rain: %i\n", rain);
-    Serial.printf("-------------------------\n");
+    // /* Monitoring all sensors data */
+    // Serial.printf("------ SENSORS DATA ------\n");
+    // Serial.printf("Temp: %0.3f\n", temperature);
+    // Serial.printf("Humidity: %0.3f\n", humidity);
+    // Serial.printf("Smoke: %i\n", smoke);
+    // Serial.printf("Flame: %i\n", flame);
+    // Serial.printf("Light: %i\n", lightLevel);
+    // Serial.printf("Rain: %i\n", rain);
+    // Serial.printf("-------------------------\n");
 
-    /* Smoke and flame alarm */
-    if (smoke > 2000 || flame == LOW) { /* True */
-      digitalWrite(BUZZER_PIN, HIGH);
-      setRGB(255, 0, 0); /* Red: fire mode */
-      Serial.println("===> WARNING!! SMOKE OR FLAME DETECTED <===");
-    } else { /* False */
-      digitalWrite(BUZZER_PIN, LOW);
+    // /* Smoke and flame alarm */
+    // if (smoke > 2000 || flame == LOW) { /* True */
+    //   digitalWrite(BUZZER_PIN, HIGH);
+    //   setRGB(255, 0, 0); /* Red: fire mode */
+    //   Serial.println("===> WARNING!! SMOKE OR FLAME DETECTED <===");
+    // } else { /* False */
+    //   digitalWrite(BUZZER_PIN, LOW);
 
-      /* Controlling windows */
-      if (rain == LOW) { /* There is rain */
-        servoWindow1.write(0);
-        servoWindow2.write(0);
-        Serial.println("===> RAIN DETECTED || CLOSING ALL WINDOWS <===");
-      } else { /* There is no rain */
-        servoWindow1.write(90);
-        servoWindow2.write(90);
-      }
+    //   /* Controlling windows */
+    //   if (rain == LOW) { /* There is rain */
+    //     servoWindow1.write(0);
+    //     servoWindow2.write(0);
+    //     Serial.println("===> RAIN DETECTED || CLOSING ALL WINDOWS <===");
+    //   } else { /* There is no rain */
+    //     servoWindow1.write(90);
+    //     servoWindow2.write(90);
+    //   }
 
-      /* Controlling fan */
-      if (temperature > 30) { /* Hot temperature */
-        analogWrite(FAN_ENA, 200);
-        digitalWrite(FAN_IN1, HIGH);
-        digitalWrite(FAN_IN2, LOW);
-        Serial.printf("===> TEMPERATURE: %0.3f || FAN ON <===\n", temperature);
-      } else { /* Normal temperature */
-        analogWrite(FAN_ENA, 0);
-        Serial.printf("===> TEMPERATURE: %0.3f || FAN OFF <===\n", temperature);
-      }
+    //   /* Controlling fan */
+    //   if (temperature > 30) { /* Hot temperature */
+    //     analogWrite(FAN_ENA, 200);
+    //     digitalWrite(FAN_IN1, HIGH);
+    //     digitalWrite(FAN_IN2, LOW);
+    //     Serial.printf("===> TEMPERATURE: %0.3f || FAN ON <===\n", temperature);
+    //   } else { /* Normal temperature */
+    //     analogWrite(FAN_ENA, 0);
+    //     Serial.printf("===> TEMPERATURE: %0.3f || FAN OFF <===\n", temperature);
+    //   }
 
-      /* Controlling lighting and the mode */
-      if (lightLevel < 1000) { /* Night mode */
-        digitalWrite(LED_FLOOR1, HIGH);
-        digitalWrite(LED_FLOOR2, HIGH);
-        digitalWrite(LED_LANDSCAPE, HIGH);
-        setRGB(0, 0, 255); /* Blue: night mode */
-        Serial.println("===> NIGHT MODE || ALL LIGHTS ON <===");
-      } else { /* Daytime mode */
-        digitalWrite(LED_FLOOR1, LOW);
-        digitalWrite(LED_FLOOR2, LOW);
-        digitalWrite(LED_LANDSCAPE, LOW);
-        setRGB(255, 255, 0); /* Yellow: daytime mode */
-        Serial.println("===> DAYTIME || ALL LIGHTS OFF <===");
-      }
+    //   /* Controlling lighting and the mode */
+    //   if (lightLevel < 1000) { /* Night mode */
+    //     digitalWrite(LED_FLOOR1, HIGH);
+    //     digitalWrite(LED_FLOOR2, HIGH);
+    //     digitalWrite(LED_LANDSCAPE, HIGH);
+    //     setRGB(0, 0, 255); /* Blue: night mode */
+    //     Serial.println("===> NIGHT MODE || ALL LIGHTS ON <===");
+    //   } else { /* Daytime mode */
+    //     digitalWrite(LED_FLOOR1, LOW);
+    //     digitalWrite(LED_FLOOR2, LOW);
+    //     digitalWrite(LED_LANDSCAPE, LOW);
+    //     setRGB(255, 255, 0); /* Yellow: daytime mode */
+    //     Serial.println("===> DAYTIME || ALL LIGHTS OFF <===");
+    //   }
 
-      /* Status of solar panel */
-      if (true == solarActive) { /* Checking the power system */
-        setRGB(0, 255, 0);       /* Green: solar mode is active */
-        Serial.println("===> SOLAR ENERGY ACTIVE <===");
-      } else { /* DO NOTHING*/
-      }
-    }
+    //   /* Status of solar panel */
+    //   if (true == solarActive) { /* Checking the power system */
+    //     setRGB(0, 255, 0);       /* Green: solar mode is active */
+    //     Serial.println("===> SOLAR ENERGY ACTIVE <===");
+    //   } else { /* DO NOTHING*/
+    //   }
+    // }
 
     if (client.connected()) { /* Checking the connectivity of the server */
       /* Sending sensors' data to using json to the terminal */
@@ -141,8 +142,14 @@ void loop() {
       client.publish(TOPIC_GAS, String(smoke).c_str());
       client.publish(TOPIC_LDR, String(lightLevel).c_str());
       client.publish(TOPIC_RAIN, String(rain).c_str());
+      client.publish(TOPIC_FLAME, String(flame).c_str());
+      client.publish(TOPIC_TEMPERATURE, String(temperature).c_str());
       client.publish(TOPIC_CURRENT, "0"); /* Just for simulation */
       client.publish(TOPIC_VOLTAGE, "0"); /* Just for simulation */
+      if (true == faceDetectionRun){
+        client.publish(TOPIC_PUSH_BUTTON_FACE_DETECTION, String(faceDetectionRun).c_str());
+      }
+      else{/* DO NOTHING */}
     } else {                              /* DO NOTHING */
     }
   }
